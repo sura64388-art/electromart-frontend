4e2afcceaeb8ac015fc35100b01009070b0c172d
Suroopika0305 <suroopikamm@gmail.com>
2026-03-10 09:14:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import React from 'react';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	base: "/",
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
 });
