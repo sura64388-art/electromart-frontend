311228cc55691a3228d9a3fd0d796a990a7c33cc
Suroopika0305 <suroopikamm@gmail.com>
2026-03-10 07:58:12 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-	withCredentials: true, // send cookies to the server
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
-export default axiosInstance;
+export default axiosInstance;;
